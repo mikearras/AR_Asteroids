ac40c265118c5a978fc0e3178a3678b8754dbfd7
Anthony Robinson <anthonyrobinson@Anthonys-MacBook-Pro.local>
2022-05-31 15:12:24 -0700

    Added bgm to game, stops on game over screen

---
+++ AR_Asteroids/Assets/Scripts/AudioManager.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    //make static instance to be able to access from anywhere
+    public static AudioManager instance;
+
+    public AudioSource bgm;
+
+    private void Awake() {
+        instance = this;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void StopBGM() {
+        bgm.Stop();
+    }
+}
