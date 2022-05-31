ed387b30149c86755245940ebb54ec814d0d4860
Mike Arras <arrasm@oregonstate.edu>
2022-05-30 17:55:12 -0700

    misc

---
+++ AR_Asteroids/Assets/Scripts/Player.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    public HealthBar healthbar;
+    public int health = 100;
+
+
+
+    private void OnTriggerEnter(Collider other) {
+
+        this.health -= 10;
+        healthbar.setHealth(this.health);
+        if (other.gameObject.tag == "asteroid")
+        {
+            Debug.Log("hit by asteroid");
+        }
+    }
+}
